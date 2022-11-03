395fca9de838497b4c472738a640ca1c711acd5c
YourLocalDragon <116558281+YourLocalDragon@users.noreply.github.com>
2022-11-03 10:47:19 +0000

    Added more story and gotten into actually making the game

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 import random
+directions = ("north","east","south","west","n","e","s","w")
 
 class Story():
-  def __init__(self):
-    print ("This is Sparta!")
+    def __init__(self):
+        print("This is Sparta!")
+
 
 class Character():
-  def __init__(self, name, health="100", attack="20", mana="100", stamina="50"):
-    self.name = name
-    self.hp = health
-    self.ap = attack
-    self.mp = mana
-    self.stamina = stamina
+    def __init__(self,
+                 name,
+                 health="100",
+                 attack="20",
+                 mana="100",
+                 stamina="50"):
+        self.name = name
+        self.hp = health
+        self.ap = attack
+        self.mp = mana
+        self.stamina = stamina
 
-  def take_dmg(self, dmg):
-    self.hp -= dmg
-    if self.hp < 0:
-      self.hp = 0
-    return self.hp
+    def take_dmg(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
 
-  def get_atk(self, defense_roll):
-    atk = random.randint(0, self.ap)
-    print(atk)
-   
-    if atk < defense_roll:
-      atk = 0
-    else:
-      atk -= defense_roll
-    return atk
+    def get_atk(self, defense_roll):
+        atk = random.randint(0, self.ap)
+        print(atk)
 
-  def is_dead(self):
-    if self.hp < 1:
-      return True
-    else:
-      return False
+        if atk < defense_roll:
+            atk = 0
+        else:
+            atk -= defense_roll
+        return atk
 
+    def is_dead(self):
+        if self.hp < 1:
+            return True
+        else:
+            return False
 
 
 #Print("Welcome " + pc.name)
@@ -44,31 +50,53 @@ class Character():
 
 
 class StoryTeller():
-  def __init__(self):
-    print("Welcome to the bad place!")
-    self.new_game()
-   
-  def new_game(self):
-    self.create_character()
 
-  def create_character(self):
-    pc_name = input("Before you enter here, you must give me your name: \n-> ")
-    pc = Character(pc_name)
+  
+    def __init__(self):
+        print("The living has no place here!\n")
+        self.new_game()
 
-  def outside_the_bad_place(self):
-    print("You stand before the bad place. Your time has come you utter disgrace!")
-    print("You are here because you are scum and must prove your worth to society.")
-    print("You must travel through the bad place and destroy the evil tyrant Lorraine!")
+    def new_game(self):
+        self.create_character()
 
-    pc_input = input("Dare you begin the trial? \n-> ")
+    def create_character(self):
+        pc_name = input("Before you enter here, you must give me your name: \n-> ")
+        pc_name = Character(pc_name)
+        self.Introduction()
+        
 
-    if pc_input == "yes":
-      pass
-    else:
-      pass
+    def Introduction(self):
+        print("\nNo living thing can enter this domain,")
+        print("You must first give me your voice")
+        print("Or you must never come near here again\n")
+
+        pc_input = input("Dare you begin the trial? \n-> ")
+
+        if pc_input == "yes":
+            print("\nYou stand there ready as you can be\n")
+            self.Start_Game()
+        else:
+            print("Your are not ready for this, come back when you are")
+            quit()
+    
+  
+    def Start_Game(self):
+      print("The Reaper takes you in as a soul")
+      print("-----------")
+      print("-----------")
+      print("-----------")
+      print("-----------\n")
+      print("you see nothing but darkness to the north, some metal plate to the east, a wall to the south and a doorway to the west\n")
+      print("Where do you wish to go?\n")
+      pc_input = input("Input, north,east,south,west\n->")
+    
+      if pc_input == ("north"):
+        print("You Fell Down A pit")
+      
+
+
 
 new_game = StoryTeller()
-
 
 #what happens if player types "yes" = Create scenario
 #Input another player choice to progress story "no"
